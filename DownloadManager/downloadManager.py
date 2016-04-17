d822fb2af5725af5e7e92d2139b74ad95d64d222
Avijeet Gupta <avijeet95@ymail.com>
2016-04-17 14:33:44 +0530

    Added Fuctionality to download latest Episode

--- DownloadManager/downloadManager.py
+++ DownloadManager/downloadManager.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time,sys,os
+from config import count
 
 def schedule():
 	#Fetches Day 
@@ -20,6 +21,7 @@ def schedule():
 	elif day == 5:
 		download("title")
 
+
 def download(title):
 	#Initialise Driver
 	driver = webdriver.Chrome()
@@ -30,23 +32,27 @@ def download(title):
 	elem.send_keys(Keys.RETURN)
 
 	elem = driver.find_element_by_class_name("title")
-	elem.click()
+	ext = elem.text
+	text = text.split(" ")
+	
+	#Checks For Latest Episode According to Config File
+	if count[title]['season'] <= int(text[-11]) and count[title]['episode'] < int(text[-9]):
+		elem.click()
 
-	elem = driver.find_element_by_id("dm2")
-	elem.click()
+		elem = driver.find_element_by_id("dm2")
+		elem.click()
 
-	#Switch To correct Window
-	ctr=0
-	for x in driver.window_handles:
-		driver.switch_to_window(driver.window_handles[ctr])
-		if driver.title=="MEGA":
-			break
-		ctr=ctr+1
-	driver.set_window_size(1124, 850)
+		ctr=0
+		for x in driver.window_handles:
+			driver.switch_to_window(driver.window_handles[ctr])
+			if driver.title=="MEGA":
+				break
+			ctr=ctr+1
+		driver.set_window_size(1124, 850)
 
-	#Executes Downloading Command in Terminal
-	cmd = "megadl '"+driver.current_url+"'"
-	os.system(cmd)
+		cmd = "megadl '"+driver.current_url+"'"
+		os.system(cmd)
+		
 	driver.close()
 
 #Initiates Downloads According to schedule
